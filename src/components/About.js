e1287deeecff8b4f2d84938471bdc884d3ff8f26
ColeHoffmann <46608689+ColeHoffmann@users.noreply.github.com>
2022-02-23 23:40:25 -0500

    Typewriter effect.

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 
 // src/components/About.js
 
@@ -11,7 +12,19 @@ export default function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm 
-              <span className = "text-green-500 hover:text-white">  Cole Hoffmann.</span> 
+              <span className = "text-green-500 hover:text-white"> 
+                <Typewriter
+                
+                
+                    onInit={(typewriter)=> {
+  
+                    typewriter.typeString("Cole Hoffmann ").start();
+                  }
+                    }
+                />
+
+                
+                    </span> 
             <br className="hidden lg:inline-block" />I am passionate about building revolutionary software utilizing blockchain technology.
           </h1>
           <p className="mb-8 leading-relaxed">
