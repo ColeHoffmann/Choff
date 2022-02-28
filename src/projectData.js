bbff5a8c59236dafaba0096b271068e00f90ac91
ColeHoffmann <46608689+ColeHoffmann@users.noreply.github.com>
2022-02-28 02:02:54 -0500

    need to fix photos. add project descriptions

---
+++ src/projectData.js
@@ -0,0 +1,36 @@
+//data for the projects I have worked on. 
+
+export const projects = [
+    {
+      title: "Decentralized Voting App",
+      subtitle: "Built on the Ethereum Blockchain using Truffle.",
+      description:
+        "This project acts as a decentralized voting app.",
+      image: "./project-1.gif",
+      link: "https://github.com/ColeHoffmann/DVote",
+    },
+    {
+      title: "r/WSB Machine Learning Stock Picker",
+      subtitle: "print: \"diamond hands\"",
+      description:
+        "Using Python and machine learning algorithms, this bot will scan the comments posted on WallStreetBets and will estimate whether or not they will go up or down.",
+      image: "./assets/wsb.gif",
+      link: "https://github.com/ColeHoffmann/WallStreetBetsAI",
+    },
+    {
+      title: "TODO",
+      subtitle: "TODO",
+      description:
+        "TODO",
+      image: "TODO",
+      link: "TODO",
+    },
+    {
+      title: "TODO",
+      subtitle: "TODO",
+      description:
+        "TODO",
+      image: "TODO",
+      link: "TODO",
+    },
+  ];
