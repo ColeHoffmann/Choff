46adc0cc6b82d2a278fa96a23606c94fc30663fa
ColeHoffmann <46608689+ColeHoffmann@users.noreply.github.com>
2022-02-27 21:24:00 -0500

    Added the better typewriter effect.

--- src/components/About.js
+++ src/components/About.js
@@ -10,22 +10,18 @@ export default function About() {
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+          <h1 className="text-xl title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm 
-              <span className = "text-green-500 hover:text-white"> 
+              <span className = "text-6xl text-green-500 hover:text-white"> 
                 <Typewriter
-                
-                
                     onInit={(typewriter)=> {
-  
                     typewriter.typeString("Cole Hoffmann ").start();
-                  }
-                    }
-                />
-
-                
+                    }}
+                /> 
+                  
+         
                     </span> 
-            <br className="hidden lg:inline-block" />I am passionate about building revolutionary software utilizing blockchain technology.
+            <br className="hidden lg:inline-block" />I like to build things. 
           </h1>
           <p className="mb-8 leading-relaxed">
             Bio. 
