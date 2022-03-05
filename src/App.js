205da69a16cb08c1d4d756cd2db908b5fd1b7471
ColeHoffmann <46608689+ColeHoffmann@users.noreply.github.com>
2022-03-04 22:24:37 -0500

    new font

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 import React from "react";
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import Intro from "./components/Intro";
 import Projects from "./components/Projects";
 import SidebarNav from "./components/SidebarNav";
 import Bio from "./components/Bio";
 
-
-
 function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-      <Intro /> 
+      <Intro />
+      <Bio />
+      <Projects />
 
-      <Bio /> 
-      <Projects /> 
-      
-      
-{/*
+      {/*
         <div> 
           <SidebarNav /> 
         </div>
 
       */}
-      
     </main>
   );
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -12,13 +12,18 @@ export default function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="text-xl title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm
-            <span className="text-6xl text-red-700 hover:text-red-600">
-              <Typewriter
-                onInit={(typewriter) => {
-                  typewriter.pauseFor(600).typeString("Cole Hoffmann.").start();
-                }}
-              />
-            </span>
+            <b>
+              <span className="text-6xl text-red-700 hover:text-red-600">
+                <Typewriter
+                  onInit={(typewriter) => {
+                    typewriter
+                      .pauseFor(600)
+                      .typeString("Cole Hoffmann.")
+                      .start();
+                  }}
+                />
+              </span>
+            </b>
             <span className="text-gray-50 text-xl ">
               {" "}
               I like to build things.{" "}
