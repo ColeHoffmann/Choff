a99fec5d2efaa6d685c51e90ff2d08c4431d1de6
ColeHoffmann <46608689+ColeHoffmann@users.noreply.github.com>
2022-03-07 22:44:32 -0500

    nav bar

--- src/components/SidebarNav.js
+++ src/components/SidebarNav.js
@@ -1,6 +1,46 @@
 import { useState } from "react";
-import React from "react";
+import About from "./Intro";
+import Projects from "./Projects";
+const Sidebar = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
 
-export default function Projects() {
-  return <div></div>;
-}
+  return (
+    <>
+      {showSidebar ? (
+        <button
+          className="flex text-4xl text-white items-center cursor-pointer fixed right-10 top-6 z-50"
+          onClick={() => setShowSidebar(!showSidebar)}
+        >
+          x
+        </button>
+      ) : (
+        <svg
+          onClick={() => setShowSidebar(!showSidebar)}
+          className="fixed  z-30 flex items-center cursor-pointer right-10 top-6"
+          fill="#2563EB"
+          viewBox="0 0 100 80"
+          width="40"
+          height="40"
+        >
+          <rect width="100" height="10"></rect>
+          <rect y="30" width="100" height="10"></rect>
+          <rect y="60" width="100" height="10"></rect>
+        </svg>
+      )}
+
+      <div
+        className={`top-0 right-0 w-[35vw] bg-gray-300  p-10 pl-20 text-white fixed h-full z-40  ease-in-out duration-300 ${
+          showSidebar ? "translate-x-0 " : "translate-x-full"
+        }`}
+      >
+        <h3 className="mt-20 text-4xl font-semibold text-white">
+          <a href="#about">bio</a>
+          <a href="#projects">projects</a>
+          <a href="contact">contact me</a>
+        </h3>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
